f1ea35109adcc1f1a0399dc6c8f486f97808cd58
vanessawilliams <vanessawilliams@localhost>
2024-01-03 21:03:15 -0500

    initial commit

---
+++ src/d/DemoD.java
@@ -0,0 +1,15 @@
+package d;
+
+public class DemoD {
+
+	public static void main(String args[]) {
+		
+		System.out.println("Thursday");
+		
+		System.out.println("day 1");
+		System.out.println("day 2");
+		System.out.println("day 3");
+		
+		
+	}
+}
